9eca5d2b7aeb87950e193609347aba232ee5bf3b
Daniel <daniel.olivqueiroz@outlook.com>
2025-09-05 10:32:12 -0300

    feat: 190 admin layout mastering forgot password page

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -29,6 +29,9 @@
               <div class="card-header"><h4>Login</h4></div>
 
               <div class="card-body">
+                @if (session('status'))
+                <p class="text-success">{{session('status')}}</p>                    
+                @endif
                 <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                     @csrf
                   <div class="form-group">
