0b12df9617eda9b0d2898cc6e3152267db002735
Daniel <daniel.olivqueiroz@outlook.com>
2025-09-18 09:57:38 -0300

    feat: 209 services section (part-5)

--- src/app/Http/Controllers/Frontend/HomeController.php
+++ src/app/Http/Controllers/Frontend/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Hero;
+use App\Models\Service;
 use App\Models\TyperTitle;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,7 @@ class HomeController extends Controller
     public function index() {
         $hero = Hero::first();
         $typerTitles = TyperTitle::all();
-        return view('frontend.home', compact('hero', 'typerTitles'));
+        $services = Service::all();
+        return view('frontend.home', compact('hero', 'typerTitles', 'services'));
     }
 }
