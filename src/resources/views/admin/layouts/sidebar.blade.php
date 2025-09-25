cfa2b55c1547a886198075065635592358e6cb69
Daniel <daniel.olivqueiroz@outlook.com>
2025-09-25 09:35:01 -0300

    feat: 230 handling form data and storing

--- src/resources/views/admin/layouts/sidebar.blade.php
+++ src/resources/views/admin/layouts/sidebar.blade.php
@@ -74,6 +74,8 @@
                 </ul>
               </li>
 
+              <li><a class="nav-link" href="{{route('admin.experience.index')}}"><i class="far fa-square"></i><span>Experience</span></a></li>
+
               {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
 
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AboutController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ExperienceController;
 use App\Http\Controllers\Admin\HeroController;
 use App\Http\Controllers\Admin\PortfolioItemController;
 use App\Http\Controllers\Admin\PortfolioSectionSetting;
@@ -75,4 +76,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
 
     /** Skill Items Route */
     Route::resource('skill-item', SkillItemController::class);
+
+    /** Experience Items Route */
+    Route::resource('experience', ExperienceController::class);
 });
