a589f5a57b985172ca12f99a652de57475e993f2
Daniel <daniel.olivqueiroz@outlook.com>
2025-10-06 10:41:35 -0300

    feat: SEO setting update page other

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\Admin\HeroController;
 use App\Http\Controllers\Admin\PortfolioItemController;
 use App\Http\Controllers\Admin\PortfolioSectionSettingController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\SeoSettingController;
 use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\SettingController;
 use App\Http\Controllers\Admin\SkillItemController;
@@ -133,4 +134,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
 
     /** General Setting Route */
     Route::resource('general-setting', GeneralSettingController::class);
+
+    /** Seo Setting Route */
+    Route::resource('seo-setting', SeoSettingController::class);
 });
