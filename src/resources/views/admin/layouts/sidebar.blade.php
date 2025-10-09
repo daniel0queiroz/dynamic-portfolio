84ead8388f84061f7f2baabd9a1595c6b0e1ddc2
Daniel <daniel.olivqueiroz@outlook.com>
2025-10-09 10:14:34 -0300

    fix: fix settings button to go to settings page

--- src/resources/views/admin/layouts/sidebar.blade.php
+++ src/resources/views/admin/layouts/sidebar.blade.php
@@ -12,7 +12,7 @@
         <a href="{{route('profile.edit')}}" class="dropdown-item has-icon">
           <i class="far fa-user"></i> Profile
         </a>
-        <a href="features-settings.html" class="dropdown-item has-icon">
+        <a href="{{ route('admin.settings.index') }}" class="dropdown-item has-icon">
           <i class="fas fa-cog"></i> Settings
         </a>
         <div class="dropdown-divider"></div>
