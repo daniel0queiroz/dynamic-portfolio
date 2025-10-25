f54e08b668f31bc3a78de22496758516fe0a3fcf
Daniel <daniel.olivqueiroz@outlook.com>
2025-10-25 14:11:43 -0300

    fix: adjust some css codes and user experience

--- src/resources/views/admin/layouts/sidebar.blade.php
+++ src/resources/views/admin/layouts/sidebar.blade.php
@@ -1,6 +1,10 @@
 <div class="navbar-bg"></div>
 <nav class="navbar navbar-expand-lg main-navbar">
-  <div class="form-inline mr-auto"></div>
+  <form class="form-inline mr-auto">
+    <ul class="navbar-nav mr-3">
+      <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+    </ul>
+  </form>
   <ul class="navbar-nav navbar-right">
     <li class="dropdown">
       <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
@@ -30,10 +34,10 @@
 <div class="main-sidebar sidebar-style-2">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="index.html">Portfolio</a>
+      <a href="/">Portfolio</a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
-      <a href="index.html">St</a>
+      <a href="index.html">PF</a>
     </div>
     <ul class="sidebar-menu">
       <li class="menu-header">Dashboard</li>
